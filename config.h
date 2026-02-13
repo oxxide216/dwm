51b362530fa7e3391d2662556c21a2aa008fecd1
oxxide216 <oxxide216@gmail.com>
2026-02-13 15:02:06 +0300

    Splitted the line

--- config.h
+++ config.h
@@ -63,7 +63,9 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bg, "-nf", col_fg, "-sb", col_acc, "-sf", col_bg, NULL };
+static const char *dmenucmd[] =
+  { "dmenu_run", "-m", dmenumon, "-fn", dmenufont,
+    "-nb", col_bg, "-nf", col_fg, "-sb", col_acc, "-sf", col_bg, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *brightnessupcmd[]  = { "brightnessctl", "set", "+5%", NULL };
 static const char *brightnessdowncmd[]  = { "brightnessctl", "set", "5%-", NULL };
